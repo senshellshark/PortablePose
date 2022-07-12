6cbaa06984fc55d1bdf81451186639a5cd8e80d3
Lacey <senshellshark@gmail.com>
2022-07-11 22:32:31 -0600

    Clear previous version

--- Viewer.gd
+++
@@ -1,8 +0,0 @@
-extends ColorRect
-
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	$MarginContainer/Counters/RepCount.text = str(int($Shown.time_left))
-	$MarginContainer/Counters/Remaining.text = "%s / %s" % [get_parent().reps, get_parent().options.max_reps]
